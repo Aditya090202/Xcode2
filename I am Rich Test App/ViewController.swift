f31fac298a336a6664791dbb43b4cb00e0244456
Aditya Mathur <aditya.mathur1292@gmail.com>
2022-05-25 01:07:08 -0400

    Committing....

---
+++ I am Rich Test App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich Test App
+//
+//  Created by Aditya  Mathur  on 5/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
